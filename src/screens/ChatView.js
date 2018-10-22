06b3c5350c8b313520d1470c256b5d9663df459d
danielxaraujo <danielxaraujo@gmail.com>
2018-10-22 19:08:13 -0300

    Correções do frontEnd

--- src/screens/ChatView.js
+++ src/screens/ChatView.js
@@ -4,211 +4,207 @@ import { GiftedChat, Bubble, SystemMessage } from 'react-native-gifted-chat';
 import CustomActions from './CustomActions';
 import CustomView from './CustomView';
 
+const server = 'http://127.0.0.1:3001/api/messages'
+const userId = '5bce2c234d5eae61d4bb6a38'
+let context = {
+	contexts: [],
+	lang: "pt-BR",
+	sessionId: "123456789"
+}
+
 export default class Example extends React.Component {
-    constructor(props) {
-        super(props);
-        this.state = {
-            messages: [],
-            loadEarlier: true,
-            typingText: null,
-            isLoadingEarlier: false,
-        };
+	constructor(props) {
+		super(props);
+		this.state = {
+			messages: [],
+			loadEarlier: true,
+			typingText: null,
+			isLoadingEarlier: false,
+		};
 
-        this._isMounted = false;
-        this.onSend = this.onSend.bind(this);
-        this.onReceive = this.onReceive.bind(this);
-        this.renderCustomActions = this.renderCustomActions.bind(this);
-        this.renderBubble = this.renderBubble.bind(this);
-        this.renderSystemMessage = this.renderSystemMessage.bind(this);
-        this.renderFooter = this.renderFooter.bind(this);
-        this.onLoadEarlier = this.onLoadEarlier.bind(this);
-    }
+		this._isMounted = false;
+		this.onSend = this.onSend.bind(this);
+		this.renderCustomActions = this.renderCustomActions.bind(this);
+		this.renderBubble = this.renderBubble.bind(this);
+		this.renderSystemMessage = this.renderSystemMessage.bind(this);
+		this.renderFooter = this.renderFooter.bind(this);
+		this.onLoadEarlier = this.onLoadEarlier.bind(this);
+	}
 
-    componentWillMount() {
-        this._isMounted = true; //Indica que o Componente está ativo ou não
-        this.setState(() => {
-            return {
-                messages: require('../../data/messages.js'),
-            };
-        });
-    }
+	async componentWillMount() {
+		this._isMounted = true; //Indica que o Componente está ativo ou não
+		const messages = await this.loadMessages()
+		this.setState({ messages })
+	}
 
-    componentWillUnmount() {
-        this._isMounted = false; //Indica que o Componente está ativo ou não
-    }
+	loadMessages = async (old = '/current') => {
+		try {
+			const response = await fetch(`${server}${old}/${context.sessionId}`, {
+				method: 'GET',
+				headers: {
+					Accept: 'application/json',
+					'Content-Type': 'application/json',
+				}
+			})
+			const messages = await response.json()
+			return this.parseMessages(messages)
+		} catch (err) {
+			console.warn(err)
+		}
+	}
 
-    onLoadEarlier() {
-        this.setState((previousState) => {
-            return {
-                isLoadingEarlier: true, //Habilita o spinner aguardando o carregamento das mensagens
-            };
-        });
+	sendMessages = async (message) => {
+		try {
+			const response = await fetch(`${server}/chatbot`, {
+				method: 'POST',
+				headers: {
+					Accept: 'application/json',
+					'Content-Type': 'application/json',
+				},
+				body: JSON.stringify({
+					context: {
+						...context,
+						query: message.text
+					},
+					userId
+				})
+			})
+			const messages = await response.json()
+			return this.parseMessages(messages)
+		} catch (err) {
+			console.warn(err)
+		}
+	}
 
-        setTimeout(() => {
-            if (this._isMounted === true) {
-                this.setState((previousState) => {
-                    return {
-                        messages: GiftedChat.prepend(previousState.messages, require('../../data/old_messages.js')),
-                        loadEarlier: false, //Desabilita o botão para carregar mensagens anteriores
-                        isLoadingEarlier: false,
-                    };
-                });
-            }
-        }, 5000); // simulating network
-    }
+	parseMessages = (messages = []) => {
+		console.log(messages)
+		for (let i = 0; i < messages.length; i++) {
+			messages[i].createdAt = new Date(messages[i].createdAt)
+			if (messages[i].user.avatar) {
+				messages[i].user.avatar = require(`../../assets/inss.png`)
+			}
+		}
+		return messages
+	}
 
-    onSend(messages = []) {
-        if (messages.length > 0) {
-            messages[0].sent = true
-            messages[0].received = true
-        }
-        this.setState((previousState) => {
-            return {
-                messages: GiftedChat.append(previousState.messages, messages),
-            };
-        });
+	componentWillUnmount() {
+		this._isMounted = false; //Indica que o Componente está ativo ou não
+	}
 
-        // for demo purpose
-        this.answerDemo(messages);
-    }
+	async onLoadEarlier() {
+		this.setState({ isLoadingEarlier: true })//Habilita o spinner aguardando o carregamento das mensagens
+		if (this._isMounted === true) {
+			const messages = await this.loadMessages('/old')
+			this.setState(previousState => {
+				return {
+					messages: GiftedChat.prepend(previousState.messages, messages),
+					loadEarlier: false, //Desabilita o botão para carregar mensagens anteriores
+					isLoadingEarlier: false,
+				};
+			});
+		}
+	}
 
-    answerDemo(messages) {
-        if (messages.length > 0) {
-            if ((messages[0].image || messages[0].location) || !this.text) {
-                this.setState((previousState) => {
-                    return {
-                        typingText: 'React Native is typing'
-                    };
-                });
-            }
-        }
+	async onSend(messages = []) {
+		if (messages.length > 0) {
+			const result = await this.sendMessages(messages[0])
+			context = result.context
+			this.setState(previousState => {
+				return {
+					messages: GiftedChat.append(previousState.messages, this.parseMessages(result.messages))
+				}
+			})
+		}
+	}
 
-        setTimeout(() => {
-            if (this._isMounted === true) {
-                if (messages.length > 0) {
-                    if (messages[0].image) {
-                        this.onReceive('Nice picture!');
-                    } else if (messages[0].location) {
-                        this.onReceive('My favorite place');
-                    } else {
-                        this.onReceive('Alright');
-                    }
-                }
-            }
+	renderCustomActions(props) {
+		return (
+			<CustomActions
+				{...props}
+			/>
+		);
+	}
 
-            this.setState((previousState) => {
-                return {
-                    typingText: null,
-                };
-            });
-        }, 3000);
-    }
+	renderBubble(props) {
+		return (
+			<Bubble
+				{...props}
+				wrapperStyle={{
+					left: {
+						backgroundColor: '#f0f0f0',
+					}
+				}}
+			/>
+		);
+	}
 
-    onReceive(text) {
-        this.setState((previousState) => {
-            return {
-                messages: GiftedChat.append(previousState.messages, {
-                    _id: Math.round(Math.random() * 1000000),
-                    text: text,
-                    createdAt: new Date(),
-                    user: {
-                        _id: 2,
-                        name: 'React Native',
-                        avatar: require('../../assets/inss.png'),
-                    },
-                }),
-            };
-        });
-    }
+	renderSystemMessage(props) {
+		return (
+			<SystemMessage
+				{...props}
+				containerStyle={{
+					marginBottom: 15,
+				}}
+				textStyle={{
+					fontSize: 14,
+				}}
+			/>
+		);
+	}
 
-    renderCustomActions(props) {
-        return (
-            <CustomActions
-                {...props}
-            />
-        );
-    }
+	renderCustomView(props) {
+		return (
+			<CustomView
+				{...props}
+			/>
+		);
+	}
 
-    renderBubble(props) {
-        return (
-            <Bubble
-                {...props}
-                wrapperStyle={{
-                    left: {
-                        backgroundColor: '#f0f0f0',
-                    }
-                }}
-            />
-        );
-    }
+	renderFooter(props) {
+		if (this.state.typingText) {
+			return (
+				<View style={styles.footerContainer}>
+					<Text style={styles.footerText}>
+						{this.state.typingText}
+					</Text>
+				</View>
+			);
+		}
+		return null;
+	}
 
-    renderSystemMessage(props) {
-        return (
-            <SystemMessage
-                {...props}
-                containerStyle={{
-                    marginBottom: 15,
-                }}
-                textStyle={{
-                    fontSize: 14,
-                }}
-            />
-        );
-    }
+	render() {
+		return (
+			<GiftedChat
+				messages={this.state.messages}
+				onSend={this.onSend}
+				loadEarlier={this.state.loadEarlier}
+				onLoadEarlier={this.onLoadEarlier}
+				isLoadingEarlier={this.state.isLoadingEarlier}
 
-    renderCustomView(props) {
-        return (
-            <CustomView
-                {...props}
-            />
-        );
-    }
+				user={{
+					_id: userId, // sent messages should have same user._id
+				}}
 
-    renderFooter(props) {
-        if (this.state.typingText) {
-            return (
-                <View style={styles.footerContainer}>
-                    <Text style={styles.footerText}>
-                        {this.state.typingText}
-                    </Text>
-                </View>
-            );
-        }
-        return null;
-    }
-
-    render() {
-        return (
-            <GiftedChat
-                messages={this.state.messages}
-                onSend={this.onSend}
-                loadEarlier={this.state.loadEarlier}
-                onLoadEarlier={this.onLoadEarlier}
-                isLoadingEarlier={this.state.isLoadingEarlier}
-
-                user={{
-                    _id: 1, // sent messages should have same user._id
-                }}
-
-                renderBubble={this.renderBubble}
-                renderSystemMessage={this.renderSystemMessage}
-                renderFooter={this.renderFooter}
-                renderActions={this.renderCustomActions}
-                renderCustomView={this.renderCustomView}
-            />
-        );
-    }
+				renderBubble={this.renderBubble}
+				renderSystemMessage={this.renderSystemMessage}
+				renderFooter={this.renderFooter}
+				renderActions={this.renderCustomActions}
+				renderCustomView={this.renderCustomView}
+			/>
+		);
+	}
 }
 
 const styles = StyleSheet.create({
-    footerContainer: {
-        marginTop: 5,
-        marginLeft: 10,
-        marginRight: 10,
-        marginBottom: 10,
-    },
-    footerText: {
-        fontSize: 14,
-        color: '#aaa',
-    },
+	footerContainer: {
+		marginTop: 5,
+		marginLeft: 10,
+		marginRight: 10,
+		marginBottom: 10,
+	},
+	footerText: {
+		fontSize: 14,
+		color: '#aaa',
+	},
 });
